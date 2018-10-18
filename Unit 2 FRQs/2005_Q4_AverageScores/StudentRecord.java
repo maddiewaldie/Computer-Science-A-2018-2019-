36c230407882ca522fd3ea08b68d6cac4f7d4b9d
Maddie Waldie <maddiewaldie@me.com>
2018-10-18 09:25:52 -0700

    working b & c for frq #4

--- Unit 2 FRQs/2005_Q4_AverageScores/StudentRecord.java
+++ Unit 2 FRQs/2005_Q4_AverageScores/StudentRecord.java
@@ -8,14 +8,14 @@
 public class StudentRecord
 {
     private int[] scores; //contains scores.length values
-                            //scores.length>1
-    
+    //scores.length>1
+
     //constructor and other data fields not shown in question
     public StudentRecord(int[] s)
     {
         scores = s;
     }
-    
+
     /**
      * returns the average (arithmetic mean) of the values in scores
      * whose subscripts are between first and last, inclusive
@@ -23,9 +23,22 @@ public class StudentRecord
      */
     public double average(int first, int last)
     {
-        /* to be implemented in part (a)*/
+        // create sum and average variables
+        double sum = 0.0;
+        double average = 0.0;
+
+        for(int i = first; i < last; i++)
+        {
+            sum += scores[i];
+        }
+
+        // Set average
+        average = sum/(last - first);
+
+        // Return average of all values in scores
+        return(average);
     }
-    
+
     /**
      * returns true if each successive value in scores is greater
      * than or equal to the previous value;
@@ -33,9 +46,26 @@ public class StudentRecord
      */
     public boolean hasImproved()
     {
-        /* to be implemented in part (b) */
+        // create variable for last value in the array
+        int lastValue = scores[0];
+
+        for (int i = 1; i < scores.length; i++)
+        {
+            if(scores[i] >= lastValue)
+            {
+                lastValue = scores[i];
+            }
+            else
+            {
+                // return false if the values are not increasing
+                return(false);
+            }
+        }
+
+        // return true as long as the values are increasing
+        return (true);
     }
-    
+
     /**
      * if the value in scores have improved, returns the average
      * of the elements in scores with indexes greater than or equal
@@ -44,6 +74,16 @@ public class StudentRecord
      */
     public double finalAverage()
     {
-        /* to be implemented in part (c) */
+        double sum = 0.0;
+        double average = 0.0;
+
+        if(hasImproved())
+        {
+            return(average(scores.length/2, scores.length));
+        }
+        else
+        {
+            return(average(0, scores.length));
+        }
     }
 }
