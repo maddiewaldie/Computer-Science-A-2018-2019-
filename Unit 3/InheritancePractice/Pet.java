54bf0b82a77aed12e29face01c0a960693447c1a
Maddie Waldie <maddiewaldie@me.com>
2018-11-28 09:25:15 -0800

    updated 2 readmes & made interface

---
+++ Unit 3/InheritancePractice/Pet.java
@@ -0,0 +1,11 @@
+/**
+ * Pet Interface
+ *
+ * @author: Madeleine Waldie
+ * @version: November 28, 2018
+ */
+public interface Pet
+{
+    public abstract void cuddle();
+    public abstract void playWithOwner();
+}
