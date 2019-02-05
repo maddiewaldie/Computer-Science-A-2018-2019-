5c11fd642c6b47d74ed85c793a651422d1f8b658
Maddie Waldie <maddiewaldie@me.com>
2019-02-05 09:18:06 -0800

    added enhanced forloop to 2d array practice

--- Unit 4/2DArrayPractice/Driver.java
+++ Unit 4/2DArrayPractice/Driver.java
@@ -19,16 +19,28 @@ public class Driver
         int numColumns = myNums[0].length;
         System.out.println("Rows: " + numRows + ", Columns: " + numColumns);
         
-        //Print out 2d array
+        //Print out 2D array w/ regular for loop
         System.out.println("myNums: " + myNums); //Prints memory location
-        System.out.print("myNums: ");
+        System.out.println("myNums: ");
         for (int a = 0; a < myNums.length; a++)
         {
             for(int b = 0; b < myNums[a].length; b++)
             {
                  System.out.print(myNums[a][b] + " ");
             }
+            System.out.println("");
         }
         System.out.println();
+        
+        //Print out 2D array w/ enhanced for loop
+        System.out.println("myNums: ");
+        for(int[] tempA : myNums)
+        {
+            for(int tempB : tempA)
+            {
+                System.out.print(tempB + " ");
+            }
+            System.out.println("");
+        }
     }
 }
