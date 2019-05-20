26b6c72804e810e45457bcf1667ae71a075cbdc9
Maddie Waldie <maddiewaldie@me.com>
2019-05-20 09:17:15 -0700

    added greeps code

--- Waldie_Maddie_Greeps/Greep.java
+++ Waldie_Maddie_Greeps/Greep.java
@@ -3,14 +3,14 @@ import java.util.List;
 /**
  * A Greep is an alien creature that likes to collect tomatoes.
  * 
- * @author (your name here)
+ * @author Madeleine Waldie
  * @version 0.1
  */
 public class Greep extends Creature
 {
     // Remember: you cannot extend the Greep's memory. So:
     // no additional fields (other than final fields) allowed in this class!
-    
+
     /**
      * Default constructor for testing purposes.
      */
@@ -19,7 +19,6 @@ public class Greep extends Creature
         this(null);
     }
 
-    
     /**
      * Create a Greep with its home space ship.
      */
@@ -27,7 +26,6 @@ public class Greep extends Creature
     {
         super(ship);
     }
-    
 
     /**
      * Do what a greep's gotta do.
@@ -40,16 +38,15 @@ public class Greep extends Creature
                 dropTomato();
             }
             else {
-                turnHome();
-                move();
+                moveGreepTomato();
             }
         }
         else {
             checkFood();
-            move();
+            moveGreepNoTomato();
         }
     }
-    
+
     /**
      * Is there any food here where we are? If so, try to load some!
      */
@@ -59,19 +56,17 @@ public class Greep extends Creature
         TomatoPile tomatoes = (TomatoPile) getOneIntersectingObject(TomatoPile.class);
         if (tomatoes != null) {
             loadTomato();
-            // Note: this attempts to load a tomato onto *another* Greep. It won't
-            // do anything if we are alone here.
+            spit("purple");
         }
     }
-        
+
     /**
      * This method specifies the name of the author (for display on the result board).
      */
     public static String getAuthorName()
     {
-        return "Anonymous";  // write your name here!
+        return "Madeleine Waldie";
     }
-
 
     /**
      * This method specifies the image we want displayed at any time. (No need 
@@ -84,4 +79,34 @@ public class Greep extends Creature
         else
             return "greep.png";
     }
+
+    public void moveGreepTomato()
+    {
+        move();
+        edgeDetector();
+        if(!seePaint("orange") && !(atWater() || atWorldEdge()) && carryingTomato())
+        {
+            turnHome();
+        }
+
+    }
+
+    public void moveGreepNoTomato()
+    {
+        if(!seePaint("purple") && !carryingTomato())
+        {
+            move();
+        }
+        edgeDetector();
+    }
+
+    public void edgeDetector()
+    {
+        if(atWater() || atWorldEdge())
+        {
+            spit("orange");
+            turn(20);
+        }
+    }
+
 }
